29aa3e44f8993d79b18f485976d7def432f29cdd
firstza <rapatmoonlight@gmail.com>
2026-01-18 14:27:45 +0700

    แลป55555555

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,64 @@
+import { camera } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/react';
+
+import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="small">
+            Lab 05 - โดย รภัสฐิดา รอดวงศ์ รหัส 673380505-4
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="small">
+              Lab 05 - โดย รภัสฐิดา รอดวงศ์ รหัส 673380505-4
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={addNewToGallery}>
+            <IonIcon icon={camera} />
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
